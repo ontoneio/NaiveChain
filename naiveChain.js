4936f8fd155033ca741b0ba61334c8859ead4215
ontoneio <ontoneio.mm@gmail.com>
2018-02-12 17:14:15 -0800

    Add calculateHash() function and generateNextBlock() function

--- naiveChain.js
+++ naiveChain.js
@@ -1,3 +1,4 @@
+//Block Structure
 class Block {
   constructor(index, previousHash, timestamp, data, hash) {
     this.index = index;
@@ -5,5 +6,17 @@ class Block {
     this.timestamp = timestamp;
     this.data = data;
     this.hash = hash.toString();
-  }
-}
+  };
+};
+// Block Hash
+const calculateHash = (index, previousHash, timestamp, data, hash) => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+};
+// Generate Block
+const generateNextBlock = (blockData) => {
+  let previousBlock = getLatestBlock();
+  let nextIndex = previousBlock.index + 1;
+  let nextTimeStamp = new Date().getTime() / 1000;
+  let nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimeStamp, blockData);
+  return new Block(nextIndex, previousBlock.hash, nextTimeStamp, blockData, nextHash);
+};
