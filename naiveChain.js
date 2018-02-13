91094b8e52e03fc6fd835185d8329c4bd1d80bee
ontoneio <ontoneio.mm@gmail.com>
2018-02-12 17:20:51 -0800

    Add getGenesisBlock() function for storing blocks

--- naiveChain.js
+++ naiveChain.js
@@ -14,9 +14,15 @@ const calculateHash = (index, previousHash, timestamp, data, hash) => {
 };
 // Generate Block
 const generateNextBlock = (blockData) => {
-  let previousBlock = getLatestBlock();
-  let nextIndex = previousBlock.index + 1;
-  let nextTimeStamp = new Date().getTime() / 1000;
-  let nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimeStamp, blockData);
-  return new Block(nextIndex, previousBlock.hash, nextTimeStamp, blockData, nextHash);
+    let previousBlock = getLatestBlock();
+    let nextIndex = previousBlock.index + 1;
+    let nextTimeStamp = new Date().getTime() / 1000;
+    let nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimeStamp, blockData);
+    return new Block(nextIndex, previousBlock.hash, nextTimeStamp, blockData, nextHash);
 };
+// Storing the blocks
+const getGenesisBlock = () => {
+    return new Block(0, "0", 1465154705, "JAM Genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+};
+const blockchain = [getGenesisBlock()];
+// Validate Integrity of Blocks
